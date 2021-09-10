1aecd7512e19af32d6dfc8a259bacdb844f71b12
shiyi <1434980328@qq.com>
2021-09-10 11:00:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Login></Login>
+<!--    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/login">Go to Foo</router-link>
+    <router-view></router-view>-->
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Login from './views/Login'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
